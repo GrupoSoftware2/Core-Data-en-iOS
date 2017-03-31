a96b495bd0840cd87d06224305f1e9d435cd131a
Admin <admin@a51-05.local>
2017-03-30 19:37:49 -0500

    Initial Commit

---
+++ Core Data en iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Core Data en iOS
+//
+//  Created by Admin on 3/30/17.
+//  Copyright © 2017 ourlimm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
